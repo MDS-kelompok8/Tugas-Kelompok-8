206706ca4dc4204215fa7619d586bc508f271fdb
Sofia Octaviana <111562104+sofiaoctaviana@users.noreply.github.com>
2023-03-28 05:24:51 +0700

    Update server.R
    update server

--- shinyapps/server.R
+++ shinyapps/server.R
@@ -2,45 +2,37 @@ connectDB <- function(){
   driver <- dbDriver('PostgreSQL')
   DB <- dbConnect(
     driver,
-    dbname="qnimzcgc", #User & Default database
-    host="topsy.db.elephantsql.com",  #Server
-    user="qnimzcgc", # User & Default database
-    password="pGEDSp-s-V32WqGMpzXLnPgMfcBfPXRz" #Password
+    dbname="rating", #User & Default database
+    host="localhost",  #Server
+    port= 5432,
+    user="postgres", # User & Default database
+    password="Sofia123" #Password
   )
 }
 
 function(input, output, session) {
-    output$tblJudul <- renderDataTable({
+  output$tblJudul <- renderDataTable({
     DB <- connectDB()
-    q1 <- "SELECT judul, tahun FROM judul;"
+    q1 <- "SELECT judul, tahun as Tahun_Rilis FROM judul;"
     judul<-dbGetQuery(DB, q1)
     dbDisconnect(DB)
     
     judul
   })
   
-  output$tblRating <- renderDataTable({
-    DB <- connectDB()
-    rating<-dbReadTable(DB, 'rating')
-    dbDisconnect(DB)
-    
-    rating
-  })
   
   output$tblPemain <- renderDataTable({
     DB <- connectDB()
-    pemain_film<-dbReadTable(DB, 'pemain_film')
+    q2 <- "SELECT j.judul as Judul_Film,
+    j.tahun as Tahun_Rilis,
+    p.pemain_film as Pemain_Film
+      FROM judul as j
+      INNER JOIN pemain_film as p
+        ON j.movie_id = p.movie_id;"
+    pemain_film<-dbGetQuery(DB, q2)
     dbDisconnect(DB)
     
     pemain_film
-  })
-  
-  output$tblDirector <- renderDataTable({
-    DB <- connectDB()
-    director<-dbReadTable(DB, 'director')
-    dbDisconnect(DB)
-    
-    director
   })
   
   output$tblGenre <- renderDataTable({
@@ -49,6 +41,36 @@ function(input, output, session) {
     dbDisconnect(DB)
     
     genre
+  })
+  
+  output$tblRating <- renderDataTable({
+    DB <- connectDB()
+    q4 <- "SELECT j.judul as Judul_Film,
+    r.imdb_rating as rating
+      FROM judul as j
+      INNER JOIN rating as r
+        ON j.movie_id = r.movie_id
+        ORDER BY rating DESC;"
+    rating<-dbGetQuery(DB, q4)
+    dbDisconnect(DB)
+    
+    rating
+  })
+  
+  output$tblDirector <- renderDataTable({
+    DB <- connectDB()
+    q5 <- "SELECT j.judul as judul_film, j.tahun as tahun_rilis,
+    CONCAT(d.first_name, ' ', d.last_name) as Sutradara, g.genre
+      FROM judul as j
+      INNER JOIN genre as g
+        ON j.movie_id = g.movie_id
+      INNER JOIN director as d
+        ON d.director_id = g.director_id
+        ORDER BY j.tahun DESC;"
+    director<-dbGetQuery(DB, q5)
+    dbDisconnect(DB)
+    
+    director
   })
   
   listJudul <- reactive({
@@ -76,4 +98,3 @@ function(input, output, session) {
   })
   
 }
-
